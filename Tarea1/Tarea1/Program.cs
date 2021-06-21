81b069f999994362676ee3438c43d5aa335b50ca
jse-balarezo <jse.balarezo@yavirac.edu.ec>
2021-06-21 13:00:40 -0500

    Cambios

--- Tarea1/Tarea1/Program.cs
+++ Tarea1/Tarea1/Program.cs
@@ -7,7 +7,9 @@ namespace Tarea1
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Elige una opción\n" +
+            while (true)
+            {
+                Console.WriteLine("Elige una opción\n" +
                 "\n1.- Doraemon" +
                 "\n2.- Thundercats" +
                 "\n3.- Super Campeones" +
@@ -15,62 +17,67 @@ namespace Tarea1
                 "\n5.- Free Fire" +
                 "\n6.- Salir");
 
-            int i1 = 0;
-            i1 = Convert.ToInt16(Console.ReadLine());
+                int i1 = 0;
+                i1 = Convert.ToInt16(Console.ReadLine());
 
-            switch (i1)
-            {
-                case 1:
-                    var cosmico = new Doraemon();
-                    cosmico.mostrarPersonaje();
-                    cosmico.mostrarNombreSerie();
-                    cosmico.mostrarCapitulos();
-                    cosmico.mostrarFechaUltimoCapitulo();
-                    cosmico.mostrarArtefactos();
-                    break;
+                switch (i1)
+                {
+                    case 1:
+                        var cosmico = new Doraemon();
+                        cosmico.mostrarPersonaje();
+                        cosmico.mostrarNombreSerie();
+                        cosmico.mostrarCapitulos();
+                        cosmico.mostrarFechaUltimoCapitulo();
+                        cosmico.mostrarArtefactos();
+                        break;
 
-                case 2:
-                    var thunder = new Thundercats();
-                    thunder.mostrarPersonaje();
-                    thunder.mostrarNombreSerie();
-                    thunder.mostrarCapitulos();
-                    thunder.mostrarEnemigos();
-                    thunder.mostrarEquipaciones();
-                    break;
+                    case 2:
+                        var thunder = new Thundercats();
+                        thunder.mostrarPersonaje();
+                        thunder.mostrarNombreSerie();
+                        thunder.mostrarCapitulos();
+                        thunder.mostrarEnemigos();
+                        thunder.mostrarEquipaciones();
+                        break;
 
-                case 3:
-                    var campeones = new superCampeones();
-                    campeones.mostrarPersonajes();
-                    campeones.mostrarNumeroDeCapítulos();
-                    campeones.mostrarNombresDeCapitulos();
-                    campeones.mostrarProductora();
-                    campeones.mostrarfechaDeCreacion();
-                    break;
+                    case 3:
+                        var campeones = new superCampeones();
+                        campeones.mostrarPersonajes();
+                        campeones.mostrarNumeroDeCapítulos();
+                        campeones.mostrarNombresDeCapitulos();
+                        campeones.mostrarProductora();
+                        campeones.mostrarfechaDeCreacion();
+                        break;
 
-                case 4:
-                    var tortuga = new TortugasNinja();
-                    tortuga.mostrarPersonaje();
-                    tortuga.mostrarNombreSerie();
-                    tortuga.mostrarCapitulos();
-                    tortuga.mostrarEnemigos();
-                    tortuga.mostrarNombreDeProductora();
-                    break;
+                    case 4:
+                        var tortuga = new TortugasNinja();
+                        tortuga.mostrarPersonaje();
+                        tortuga.mostrarNombreSerie();
+                        tortuga.mostrarCapitulos();
+                        tortuga.mostrarEnemigos();
+                        tortuga.mostrarNombreDeProductora();
+                        break;
 
-                case 5:
-                    var free = new FreeFire();
-                    free.mostrarPersonaje();
-                    free.mostrarNombreDeVideoJuego();
-                    free.mostrarNombreDeCreador();
-                    free.mostrarNumeroDeActualizaciones();
-                    free.mostrarLugaresDeMapa();
+                    case 5:
+                        var free = new FreeFire();
+                        free.mostrarPersonaje();
+                        free.mostrarNombreDeVideoJuego();
+                        free.mostrarNombreDeCreador();
+                        free.mostrarNumeroDeActualizaciones();
+                        free.mostrarLugaresDeMapa();
+                        break;
+                                                
+                    default:
+                        Console.WriteLine("No ha seleccionado una opción correcta");
+                        break;
+                }
+                if (i1 == 6)
+                {
+                    Console.WriteLine("Gracias por utilizar nuestro Programa");
                     break;
+                }
 
-                default:
-                    Console.WriteLine("No ha seleccionado una opción correcta");
-                    break;
             }
-            Console.ReadKey();
-
         }
     }
 
